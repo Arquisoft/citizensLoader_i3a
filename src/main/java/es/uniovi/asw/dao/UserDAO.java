5202ff10ea099f27c210ad99119495e5af200d13
Andrei Manu <uo240704@uniovi.es>
2017-02-13 10:50:24 +0100

    Logging

--- src/main/java/es/uniovi/asw/dao/UserDAO.java
+++ src/main/java/es/uniovi/asw/dao/UserDAO.java
@@ -1,8 +1,13 @@
 package es.uniovi.asw.dao;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import es.uniovi.asw.model.UserModel;
 
@@ -33,9 +38,34 @@ public class UserDAO implements IUserDAO {
 		}
 		
 	}
+	private boolean Exists(UserModel user) {
+		String sql = "SELECT * FROM ASW WHERE ID = ?";
+		try {
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, user.getID());
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) return true;
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 	@Override
 	public void save(UserModel user) {
-		
+		if(Exists(user)) {
+			System.out.println("User already exists: " + user.toString());
+			File log = new File("Logs/log.txt");
+			try {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(log));
+				bw.append("User already exists: " + user.getfName() + " " + user.getlName() + " " + user.getID() + "\n");
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return;
+		}
 		String sql = "INSERT INTO ASW(FName, LName, Email, DOB, Address, Nationality, ID, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 		try {
 			PreparedStatement stmt = conn.prepareStatement(sql);
